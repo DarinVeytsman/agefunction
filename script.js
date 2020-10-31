9c0b56327f4b6edd2c44b42ae4a298b76b7ec81c
DarinVeytsman <72801650+DarinVeytsman@users.noreply.github.com>
2020-10-31 09:52:08 +0500

    git

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let name= prompt('Введите свое имя');
+let num = +prompt('Введиет свой год рождения');
+let year = +prompt('Введите нынешний год');
+
+
+function sum(num1,num2) {
+    let res = year - num;
+    return res;
+}
+let sumAnswer = sum();
+alert((' Вас зовут ' + name ) + (' Вам ' + sumAnswer));
+
+
+
